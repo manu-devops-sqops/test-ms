57d9a9461b2c881f9b3c843b241439cbcb41dda3
manu-devops-sqops <manu@squareops.com>
2025-06-05 12:13:42 +0530

    Add files via upload

--- test/README.md
+++ test/README.md
@@ -1,1 +1,72 @@
+## EKS Example
 
+This directory contains a complete example that demonstrates the usage of the Terraform AWS EKS module to provision an EKS cluster and associated resources in AWS. The example showcases a fully configured EKS environment with multiple node groups, custom tags, and Kubernetes labels.
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+## Requirements
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.0 |
+| <a name="requirement_aws"></a> [aws](#requirement\_aws) | >= 4.47 |
+| <a name="requirement_kubernetes"></a> [kubernetes](#requirement\_kubernetes) | >= 2.10 |
+| <a name="requirement_time"></a> [time](#requirement\_time) | >= 0.9 |
+| <a name="requirement_tls"></a> [tls](#requirement\_tls) | >= 3.0 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_aws"></a> [aws](#provider\_aws) | >= 4.47 |
+
+## Modules
+
+| Name | Source | Version |
+|------|--------|---------|
+| <a name="module_kms"></a> [kms](#module\_kms) | terraform-aws-modules/kms/aws | 3.1.0 |
+| <a name="module_key_pair_vpn"></a> [key\_pair\_vpn](#module\_key\_pair\_vpn) | squareops/keypair/aws | 1.0.2 |
+| <a name="module_key_pair_eks"></a> [key\_pair\_eks](#module\_key\_pair\_eks) | squareops/keypair/aws | 1.0.2 |
+| <a name="module_vpc"></a> [vpc](#module\_vpc) | squareops/vpc/aws | 3.4.1 |
+| <a name="module_eks"></a> [eks](#module\_eks) | squareops/eks/aws | 5.4.2 |
+| <a name="module_managed_node_group_addons"></a> [managed\_node\_group\_addons](#module\_managed\_node\_group\_addons) | squareops/eks/aws//modules/managed-nodegroup | 5.4.2 |
+| <a name="module_fargate_profle"></a> [fargate\_profle](#module\_fargate\_profle) | squareops/eks/aws//modules/fargate-profile | n/a |
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [aws_caller_identity.current](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity) | data source |
+
+## Inputs
+
+No inputs.
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| <a name="output_region"></a> [region](#output\_region) | AWS region in which the EKS cluster is created. |
+| <a name="output_vpc_id"></a> [vpc\_id](#output\_vpc\_id) | The ID of the VPC |
+| <a name="output_vpc_cidr_block"></a> [vpc\_cidr\_block](#output\_vpc\_cidr\_block) | The IPv4 network range assigned to the VPC. |
+| <a name="output_public_subnets"></a> [public\_subnets](#output\_public\_subnets) | List of the public subnets ID's created within the VPC. |
+| <a name="output_private_subnets"></a> [private\_subnets](#output\_private\_subnets) | List of the private subnets ID's created within the VPC. |
+| <a name="output_database_subnets"></a> [database\_subnets](#output\_database\_subnets) | List of the database subnets ID's created within the VPC. |
+| <a name="output_intra_subnets"></a> [intra\_subnets](#output\_intra\_subnets) | List of the intra subnets ID's created within the VPC. |
+| <a name="output_vpn_host_public_ip"></a> [vpn\_host\_public\_ip](#output\_vpn\_host\_public\_ip) | Public IP address of the VPN server instance created within the VPC. |
+| <a name="output_vpn_security_group"></a> [vpn\_security\_group](#output\_vpn\_security\_group) | Security group assigned to the VPN server instance within the VPC. |
+| <a name="output_cluster_name"></a> [cluster\_name](#output\_cluster\_name) | Name of the Kubernetes cluster. |
+| <a name="output_cluster_endpoint"></a> [cluster\_endpoint](#output\_cluster\_endpoint) | Endpoint URL for the EKS control plane. |
+| <a name="output_cluster_security_group_id"></a> [cluster\_security\_group\_id](#output\_cluster\_security\_group\_id) | Security group IDs that are attached to the control plane of the EKS cluster. |
+| <a name="output_cluster_arn"></a> [cluster\_arn](#output\_cluster\_arn) | ARN of the EKS Cluster. |
+| <a name="output_cluster_oidc_issuer_url"></a> [cluster\_oidc\_issuer\_url](#output\_cluster\_oidc\_issuer\_url) | URL of the OpenID Connect identity provider on the EKS cluster. |
+| <a name="output_worker_iam_role_arn"></a> [worker\_iam\_role\_arn](#output\_worker\_iam\_role\_arn) | ARN of the IAM role assigned to the EKS worker nodes. |
+| <a name="output_worker_iam_role_name"></a> [worker\_iam\_role\_name](#output\_worker\_iam\_role\_name) | Name of the IAM role assigned to the EKS worker nodes. |
+| <a name="output_kms_key_arn"></a> [kms\_key\_arn](#output\_kms\_key\_arn) | ARN of the KMS key that is used by the EKS cluster. |
+| <a name="output_kms_policy_arn"></a> [kms\_policy\_arn](#output\_kms\_policy\_arn) | ARN of the KMS policy that is used by the EKS cluster. |
+| <a name="output_managed_ng_node_group_arn"></a> [managed\_ng\_node\_group\_arn](#output\_managed\_ng\_node\_group\_arn) | ARN for the nodegroup |
+| <a name="output_managed_ng_min_node"></a> [managed\_ng\_min\_node](#output\_managed\_ng\_min\_node) | Minimum node of managed node group |
+| <a name="output_managed_ng_max_node"></a> [managed\_ng\_max\_node](#output\_managed\_ng\_max\_node) | Maximum node of managed node group |
+| <a name="output_managed_ng_desired_node"></a> [managed\_ng\_desired\_node](#output\_managed\_ng\_desired\_node) | Desired node of managed node group |
+| <a name="output_managed_ng_capacity_type"></a> [managed\_ng\_capacity\_type](#output\_managed\_ng\_capacity\_type) | Capacity type of managed node |
+| <a name="output_managed_ng_instance_types"></a> [managed\_ng\_instance\_types](#output\_managed\_ng\_instance\_types) | Instance types of managed node |
+| <a name="output_managed_ng_disk_size"></a> [managed\_ng\_disk\_size](#output\_managed\_ng\_disk\_size) | Disk size of node in managed node group |
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
---
+++ test/main.tf
@@ -0,0 +1,242 @@
+locals {
+  region                               = "us-east-2"
+  kms_deletion_window_in_days          = 7
+  kms_key_rotation_enabled             = true
+  is_enabled                           = true
+  multi_region                         = false
+  environment                          = "stagems"
+  name                                 = "sqopsms"
+  auto_assign_public_ip                = true
+  vpc_availability_zones               = ["us-east-2a", "us-east-2b"]
+  vpc_public_subnet_enabled            = true
+  vpc_private_subnet_enabled           = true
+  vpc_database_subnet_enabled          = true
+  vpc_intra_subnet_enabled             = true
+  vpc_one_nat_gateway_per_az           = false
+  vpn_server_instance_type             = "t3a.small"
+  vpc_flow_log_enabled                 = false
+  kms_user                             = null
+  vpc_cidr                             = "10.10.0.0/16"
+  vpn_server_enabled                   = false
+  cluster_version                      = "1.32"
+  cluster_log_types                    = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+  cluster_log_retention_in_days        = 30
+  managed_ng_capacity_type             = "SPOT" # Choose the capacity type ("SPOT" or "ON_DEMAND")
+  cluster_endpoint_private_access      = true
+  cluster_endpoint_public_access       = true
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+  ebs_volume_size                      = 50
+  fargate_profile_name                 = "app"
+  vpc_s3_endpoint_enabled              = false
+  vpc_ecr_endpoint_enabled             = false
+  vpc_public_subnets_counts            = 2
+  vpc_private_subnets_counts           = 2
+  vpc_database_subnets_counts          = 2
+  vpc_intra_subnets_counts             = 2
+  launch_template_name                 = "launch-template-namems"
+  additional_aws_tags = {
+    Owner       = "Organization_name"
+    Expires     = "Never"
+    Department  = "Engineering"
+    Product     = ""
+    Environment = local.environment
+  }
+  aws_managed_node_group_arch = "amd64" #Enter your linux arch (Example:- arm64 or amd64)
+  current_identity            = data.aws_caller_identity.current.arn
+  enable_bottlerocket_ami     = false
+}
+
+data "aws_caller_identity" "current" {}
+
+module "kms" {
+  source                  = "terraform-aws-modules/kms/aws"
+  version                 = "3.1.0"
+  deletion_window_in_days = local.kms_deletion_window_in_days
+  description             = "Symetric Key to Enable Encryption at rest using KMS services."
+  enable_key_rotation     = local.kms_key_rotation_enabled
+  is_enabled              = local.is_enabled
+  key_usage               = "ENCRYPT_DECRYPT"
+  multi_region            = local.multi_region
+
+  # Policy
+  enable_default_policy                  = true
+  key_owners                             = [local.current_identity]
+  key_administrators                     = local.kms_user == null ? ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling", "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS", local.current_identity] : local.kms_user
+  key_users                              = local.kms_user == null ? ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling", "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS", local.current_identity] : local.kms_user
+  key_service_users                      = local.kms_user == null ? ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling", "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS", local.current_identity] : local.kms_user
+  key_symmetric_encryption_users         = [local.current_identity]
+  key_hmac_users                         = [local.current_identity]
+  key_asymmetric_public_encryption_users = [local.current_identity]
+  key_asymmetric_sign_verify_users       = [local.current_identity]
+  key_statements = [
+    {
+      sid    = "AllowCloudWatchLogsEncryption",
+      effect = "Allow"
+      actions = [
+        "kms:Encrypt*",
+        "kms:Decrypt*",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:Describe*"
+      ]
+      resources = ["*"]
+
+      principals = [
+        {
+          type        = "Service"
+          identifiers = ["logs.${local.region}.amazonaws.com"]
+        }
+      ]
+    }
+  ]
+  # Aliases
+  aliases                 = ["${local.name}-KMS"]
+  aliases_use_name_prefix = true
+}
+
+module "key_pair_vpn" {
+  source             = "squareops/keypair/aws"
+  version            = "1.0.2"
+  count              = local.vpn_server_enabled ? 1 : 0
+  key_name           = format("%s-%s-vpn", local.environment, local.name)
+  environment        = local.environment
+  ssm_parameter_path = format("%s-%s-vpn", local.environment, local.name)
+}
+
+module "key_pair_eks" {
+  source             = "squareops/keypair/aws"
+  version            = "1.0.2"
+  key_name           = format("%s-%s-eks", local.environment, local.name)
+  environment        = local.environment
+  ssm_parameter_path = format("%s-%s-eks", local.environment, local.name)
+}
+
+module "vpc" {
+  source                                          = "squareops/vpc/aws"
+  version                                         = "3.4.1"
+  name                                            = local.name
+  region                                          = local.region
+  vpc_cidr                                        = local.vpc_cidr
+  environment                                     = local.environment
+  vpn_key_pair_name                               = local.vpn_server_enabled ? module.key_pair_vpn[0].key_pair_name : null
+  availability_zones                              = local.vpc_availability_zones
+  intra_subnet_enabled                            = local.vpc_intra_subnet_enabled
+  public_subnet_enabled                           = local.vpc_public_subnet_enabled
+  auto_assign_public_ip                           = local.auto_assign_public_ip
+  private_subnet_enabled                          = local.vpc_private_subnet_enabled
+  one_nat_gateway_per_az                          = local.vpc_one_nat_gateway_per_az
+  database_subnet_enabled                         = local.vpc_database_subnet_enabled
+  vpn_server_enabled                              = local.vpn_server_enabled
+  vpn_server_instance_type                        = "t3a.small"
+  vpc_s3_endpoint_enabled                         = local.vpc_s3_endpoint_enabled
+  vpc_ecr_endpoint_enabled                        = local.vpc_ecr_endpoint_enabled
+  flow_log_enabled                                = local.vpc_flow_log_enabled
+  flow_log_max_aggregation_interval               = 60 # In seconds
+  flow_log_cloudwatch_log_group_skip_destroy      = false
+  flow_log_cloudwatch_log_group_retention_in_days = 90
+  flow_log_cloudwatch_log_group_kms_key_arn       = module.kms.key_arn #Enter your kms key arn
+}
+
+module "eks" {
+  source               = "squareops/eks/aws"
+  version              = "5.4.4"
+  access_entry_enabled = true
+  access_entries = {
+    "example" = {
+      kubernetes_groups = []
+      principal_arn     = "arn:aws:iam::381491984451:role/testing_rr-cluster-20250320130511795600000004"
+      policy_associations = {
+        example = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            namespaces = []
+            type       = "cluster"
+          }
+        }
+      }
+    }
+  }
+  enable_cluster_creator_admin_permissions = true
+  authentication_mode                      = "API_AND_CONFIG_MAP"
+  name                                     = local.name
+  vpc_id                                   = module.vpc.vpc_id
+  environment                              = local.environment
+  kms_key_arn                              = module.kms.key_arn
+  cluster_version                          = local.cluster_version
+  cluster_log_types                        = local.cluster_log_types
+  vpc_private_subnet_ids                   = module.vpc.private_subnets
+  cluster_log_retention_in_days            = local.cluster_log_retention_in_days
+  cluster_endpoint_public_access           = local.cluster_endpoint_public_access
+  cluster_endpoint_public_access_cidrs     = local.cluster_endpoint_public_access_cidrs
+  cluster_endpoint_private_access          = local.cluster_endpoint_private_access
+  nodes_keypair_name                       = module.key_pair_eks.key_pair_name
+  cluster_security_group_additional_rules = {
+    ingress_port_mgmt_tcp = {
+      description = "mgmt vpc cidr"
+      protocol    = "tcp"
+      from_port   = 443
+      to_port     = 443
+      type        = "ingress"
+      cidr_blocks = ["10.10.0.0/16"]
+    }
+  }
+  enable_vpc_cni_addon = true
+  vpc_cni_version      = "v1.19.3-eksbuild.1"
+  tags                 = local.additional_aws_tags
+}
+
+module "managed_node_group_addons" {
+  source                      = "squareops/eks/aws//modules/managed-nodegroup"
+  version                     = "5.4.4"
+  depends_on                  = [module.eks]
+  managed_ng_name             = "Infra"
+  managed_ng_min_size         = 2
+  managed_ng_max_size         = 5
+  managed_ng_desired_size     = 2
+  vpc_subnet_ids              = [module.vpc.private_subnets[0]]
+  environment                 = local.environment
+  managed_ng_kms_key_arn      = module.kms.key_arn
+  managed_ng_capacity_type    = local.managed_ng_capacity_type
+  managed_ng_ebs_volume_size  = local.ebs_volume_size
+  managed_ng_ebs_volume_type  = "gp3"
+  managed_ng_ebs_encrypted    = true
+  managed_ng_instance_types   = ["t3a.large", "t3.large", "t3.medium"] # Pass instance type according to the ami architecture.
+  managed_ng_kms_policy_arn   = module.eks.kms_policy_arn
+  associate_public_ip_address = false
+  enable_coredns_addon        = true
+  managed_ng_node_autorepair = {
+    enabled                            = false
+    enable_node_monitoring_agent_addon = true
+  }
+  eks_cluster_name              = module.eks.cluster_name
+  worker_iam_role_name          = module.eks.worker_iam_role_name
+  worker_iam_role_arn           = module.eks.worker_iam_role_arn
+  eks_nodes_keypair_name        = module.key_pair_eks.key_pair_name
+  managed_ng_pod_capacity       = 90
+  managed_ng_monitoring_enabled = true
+  launch_template_name          = local.launch_template_name
+  k8s_labels = {
+    "Addons-Services" = "true"
+  }
+  tags                        = local.additional_aws_tags
+  custom_ami_id               = ""                                # Optional, if not passed terraform will automatically select the latest supported ami id
+  aws_managed_node_group_arch = local.aws_managed_node_group_arch # optional if "custom_ami_id" is passed
+  enable_bottlerocket_ami     = local.enable_bottlerocket_ami     # Set it to false if using Amazon Linux AMIs
+  bottlerocket_node_config = {
+    bottlerocket_eks_node_admin_container_enabled = false
+    bottlerocket_eks_enable_control_container     = true
+  }
+}
+
+module "fargate_profle" {
+  source               = "squareops/eks/aws//modules/fargate-profile"
+  depends_on           = [module.vpc, module.eks]
+  fargate_profile_name = local.fargate_profile_name
+  fargate_subnet_ids   = [module.vpc.private_subnets[0]]
+  environment          = local.environment
+  eks_cluster_name     = module.eks.cluster_name
+  fargate_namespace    = "fargate"
+  k8s_labels = {
+    "App-Services" = "app"
+  }
+}
